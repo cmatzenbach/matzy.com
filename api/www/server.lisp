894b282e6de2822229c23ebbda3ddc3b172cf642
Chris Matzenbach <cmatzenbach@gmail.com>
2020-04-13 00:42:50 -0500

    Removed unnecessary copy after shifting around

--- api/www/server.lisp
+++
@@ -1,6 +0,0 @@
-(ql:quickload :hunchentoot)
-(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242))
-
-(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
-  (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Hey~@[ ~A~]!" name))
